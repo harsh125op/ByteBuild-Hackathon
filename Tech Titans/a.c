dd2bba6ed43b87715871194791f4279d8bf872f0
Manshi prajapati <manshiprajapati777@gmail.com>
2025-03-19 16:16:02 +0530

    4th change

--- Tech Titans/a.c
+++ Tech Titans/a.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -9,13 +10,23 @@ struct securityguard{
     char email[50];
     char previous_place[50];
     int years_of_experience;
-    char status[20]; 
+    char current_deployment[20]; 
     char city[60];
     char country[60];
     double Latitude;
     double Longitude;
 };
-
+typedef enum{ ADMIN, POLICE, SOCIETY_OWNER, FIELD_USER, UNKNOWN }Role;
+struct User {
+    char username[50];
+    Role role;
+};
+struct User users[100] = {
+    {"admin", ADMIN},
+    {"police", POLICE},
+    {"owner", SOCIETY_OWNER},
+    {"fielduser", FIELD_USER}
+};
 
 void parseLocation(struct securityguard *sg, char *buffer) {
     sscanf(strstr(buffer, "\"city\":\"") + 8, "%[^\"]", sg->city);
@@ -30,6 +41,14 @@ void printGuards(struct securityguard sg[], int count) {
         printf("Location: %s, %s\n", sg[i].city, sg[i].country);
         printf("Coordinates: Latitude: %lf, Longitude: %lf\n",sg[i].Latitude, sg[i].Longitude);
     }
+}
+Role getUserRole(char *username,int n) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(username, users[i].username) == 0) {
+            return users[i].role;
+        }
+    }
+    return UNKNOWN;  
 }
 
 int main(){
@@ -50,12 +69,18 @@ int main(){
         scanf("%s", sg[i].previous_place);
         printf("Enter the years of experience of the security guard: ");
         scanf("%d", &sg[i].years_of_experience);
-        printf("Enter the status of the security guard: ");
-        scanf("%s", sg[i].status);
+        printf("Enter the current deployment of the security guard: ");
+        scanf("%s", sg[i].current_deployment);
 		system("curl -s http://ip-api.com/json > location.json");
 		
 }
-    printf("\n The details of the security guards are: \n");
+   
+	char username[50];
+    printf("option:\n1.admin\n2.police\n3.owner\n4.fielduser\nEnter your username:");
+    scanf("%s", username);
+    Role userRole = getUserRole(username,n);
+
+	printf("\n The details of the security guards are: \n");
     for(int i=0; i<n; i++){
         printf("Name: %s\n", sg[i].name);
         printf("Address: %s\n", sg[i].address);
@@ -63,11 +88,10 @@ int main(){
         printf("Email: %s\n", sg[i].email);
         printf("Previous Place: %s\n", sg[i].previous_place);
         printf("Years of Experience: %d\n", sg[i].years_of_experience);
-        printf("Status: %s\n", sg[i].status);
+        printf("Status: %s\n", sg[i].current_deployment);
 		printf("tracked location is saved to location.json\n");
 		system("curl -s http://ip-api.com/json > location.json");
-        
-        FILE *file = fopen("location.json", "r");
+		FILE *file = fopen("location.json", "r");
         if (!file) {
             printf("Error: Cannot open location.json\n");
             return 1;
@@ -75,9 +99,30 @@ int main(){
         char buffer[1024];
         fread(buffer, sizeof(buffer), 1, file);
         fclose(file);
-        parseLocation(&sg[i], buffer);	//hhh
+        parseLocation(&sg[i], buffer);	
     }
-    printGuards(sg, n);
+
+		printGuards(sg, n);
+
+    switch (userRole) 
+	{
+        case ADMIN:
+            printf("Admin Access: Manage guards & assign duties.\n");
+            break;
+        case POLICE:
+            printf("Police Access: View & verify guards.\n");
+            break;
+        case SOCIETY_OWNER:
+            printf("Society Owner Access: View guards & report issues.\n");
+            break;
+        case FIELD_USER:
+            printf("Field User Access: View only personal duty.\n");
+            break;
+        default:
+            printf("Access Denied: Unknown user.\n");
+            break;
+    }
+
     return 0;
 
 }
